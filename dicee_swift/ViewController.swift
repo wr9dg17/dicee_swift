0ebb39ea7a304ffa002d31ae74ed98adab420a82
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-07-03 21:40:12 +0400

    Initial Commit

---
+++ dicee_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicee_swift
+//
+//  Created by Farid Hamzayev on 03.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
