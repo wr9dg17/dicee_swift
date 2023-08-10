f518f31c32988e3bf825f463367c852593bb261c
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-08-10 16:01:57 +0400

    ...

--- dicee_swift/ViewController.swift
+++ dicee_swift/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    var dices: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(imageLiteralResourceName: dices.randomElement()!)
+        diceImageView2.image = UIImage(imageLiteralResourceName: dices.randomElement()!)
     }
-
-
 }
-
